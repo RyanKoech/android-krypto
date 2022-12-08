1bc740a26fe16cfbe6981d8938a65118a0b09019
RyanKoech <sirryankoech@gmail.com>
2022-12-08 07:56:04 +0300

    Moved theme files

--- common/src/main/java/com/ryankoech/krypto/common/presentation/theme/Type.kt
+++ common/src/main/java/com/ryankoech/krypto/common/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.ryankoech.krypto.presentation.common.theme
+package com.ryankoech.krypto.common.presentation.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
