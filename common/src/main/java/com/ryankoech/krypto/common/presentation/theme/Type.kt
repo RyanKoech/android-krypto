e2c1d641ab36e768d27fa43b8c5f2b431756eda7
RyanKoech <sirryankoech@gmail.com>
2022-12-10 13:10:43 +0300

    Added main app TextStyles

--- common/src/main/java/com/ryankoech/krypto/common/presentation/theme/Type.kt
+++ common/src/main/java/com/ryankoech/krypto/common/presentation/theme/Type.kt
@@ -2,27 +2,59 @@ package com.ryankoech.krypto.common.presentation.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ke.co.sevenskies.common.R
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_extralight, weight = FontWeight.ExtraLight),
+    Font(R.font.montserrat_light, weight = FontWeight.Light),
+    Font(R.font.montserrat_regular, weight = FontWeight.Normal),
+    Font(R.font.montserrat_medium, weight = FontWeight.Medium),
+    Font(R.font.montserrat_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.montserrat_bold, weight = FontWeight.Bold),
+    Font(R.font.montserrat_extrabold, weight = FontWeight.ExtraBold),
+    Font(R.font.montserrat_black, weight = FontWeight.Black),
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+    ),
+    h1 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+    ),
+    h2 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+    ),
+    h3 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+    ),
+    h4 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontSize = 10.sp,
+    ),
+    button = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+    ),
 )
