d7cf4a32fc4585bb8f4fdc08e84cd225fa43a368
RyanKoech <sirryankoech@gmail.com>
2022-12-10 12:57:15 +0300

    Updated Theme location import

--- app/src/main/java/com/ryankoech/krypto/presentation/MainActivity.kt
+++ app/src/main/java/com/ryankoech/krypto/presentation/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ryankoech.krypto.presentation.common.theme.KryptoTheme
+import com.ryankoech.krypto.common.presentation.theme.KryptoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
