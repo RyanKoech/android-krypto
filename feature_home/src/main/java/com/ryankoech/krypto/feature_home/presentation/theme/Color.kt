f769b4b55e451d14f4ef0e1c260adb16a64769ca
RyanKoech <sirryankoech@gmail.com>
2022-12-12 06:35:10 +0300

    Created color resources

---
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/presentation/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.ryankoech.krypto.feature_home.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val creditCardGreen = Color(0xff7cf057)
+val creditCardYellow = Color(0xffe5ff7b)
+val creditCardBlue = Color(0xff6bcfda)
+val displayCurrencyBlue = Color(0xffc4f0bb)
