bba1acdb90db70afd37d758db9635b6c02f8e705
RyanKoech <sirryankoech@gmail.com>
2022-12-12 20:53:46 +0300

    Added add color and updated naming

--- feature_home/src/main/java/com/ryankoech/krypto/feature_home/presentation/theme/Color.kt
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/presentation/theme/Color.kt
@@ -2,7 +2,8 @@ package com.ryankoech.krypto.feature_home.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val creditCardGreen = Color(0xff7cf057)
-val creditCardYellow = Color(0xffe5ff7b)
-val creditCardBlue = Color(0xff6bcfda)
-val displayCurrencyBlue = Color(0xffc4f0bb)
+val limeGreen400 = Color(0xff7cf057)
+val limeYellow300 = Color(0xffe5ff7b)
+val skyBlue300 = Color(0xff6bcfda)
+val teaGreen200 = Color(0xffc4f0bb)
+val maroon50 = Color(0xfffedcdb)
