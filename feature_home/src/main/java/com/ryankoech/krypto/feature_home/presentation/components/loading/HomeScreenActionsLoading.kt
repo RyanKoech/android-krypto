fe6bc1c7ea97191c5f34db74595eec02e36e25b1
RyanKoech <sirryankoech@gmail.com>
2022-12-15 16:16:36 +0300

    Implemented CreditCard and HomeScreenActions loading composables

---
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/presentation/components/loading/HomeScreenActionsLoading.kt
@@ -0,0 +1,82 @@
+package com.ryankoech.krypto.feature_home.presentation.components.loading
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ryankoech.krypto.common.presentation.components.loading.loadingEffect
+import com.ryankoech.krypto.common.presentation.components.loading.LoadingText
+import com.ryankoech.krypto.common.presentation.theme.KryptoTheme
+
+@Composable
+fun HomeScreenActionsLoading(
+    brush : Brush,
+    modifier: Modifier = Modifier
+) {
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        HomeScreenActionItemLoading(brush)
+        HomeScreenActionItemLoading(brush)
+        HomeScreenActionItemLoading(brush)
+    }
+
+}
+
+@Composable
+fun HomeScreenActionItemLoading(
+    brush: Brush,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+
+        Spacer(
+            modifier = Modifier
+                .size(40.dp)
+                .background(
+                    brush = brush,
+                    shape = MaterialTheme.shapes.small
+                )
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        LoadingText(50.dp, brush)
+
+    }
+
+}
+
+@Preview
+@Composable
+fun HomeScreenActionsLoadingPreview() {
+
+    KryptoTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            val brush = loadingEffect()
+
+            Column {
+                HomeScreenActionsLoading(brush = brush)
+            }
+
+        }
+    }
+
+}
