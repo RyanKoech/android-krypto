c6f12dee47fd285d4d07538a53f4460b5003b0ce
RyanKoech <sirryankoech@gmail.com>
2022-12-15 13:15:33 +0300

    Implemented ErrorScreen Composable

---
+++ common/src/main/java/com/ryankoech/krypto/common/presentation/ErrorScreen.kt
@@ -0,0 +1,53 @@
+package com.ryankoech.krypto.common.presentation
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ryankoech.krypto.common.presentation.components.GifImage
+import com.ryankoech.krypto.common.presentation.components.KryptoButton
+
+@Composable
+fun ErrorScreen(
+    modifier : Modifier = Modifier,
+    onButtonClick : () -> Unit,
+    messageText : String,
+    buttonText : String,
+    @DrawableRes res : Int,
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Box(modifier = Modifier.size(112.dp)) {
+            GifImage(
+                res = res
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = messageText,
+            style = MaterialTheme.typography.body1
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        KryptoButton(
+            text = buttonText,
+            onClick = onButtonClick
+        )
+
+    }
+
+
+}
