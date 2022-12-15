f05ebe6c46a868666f2a10fb31463e2dead7bc7c
RyanKoech <sirryankoech@gmail.com>
2022-12-15 13:15:14 +0300

    Implemented GifImage Composable

---
+++ common/src/main/java/com/ryankoech/krypto/common/presentation/components/GifImage.kt
@@ -0,0 +1,42 @@
+package com.ryankoech.krypto.common.presentation.components
+
+import android.os.Build
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+import coil.size.Size
+
+@Composable
+fun GifImage(
+    modifier: Modifier = Modifier,
+    @DrawableRes res : Int
+) {
+
+    val context = LocalContext.current
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            if (Build.VERSION.SDK_INT >= 28) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
+        .build()
+
+    Image(
+        painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(context).data(data = res).apply(block = {
+                size(Size.ORIGINAL)
+            }).build(), imageLoader = imageLoader
+        ),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
