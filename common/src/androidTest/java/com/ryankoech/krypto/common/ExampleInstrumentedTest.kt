6afbc55bfa8c11d1e03c4805d2f4a24f98e8568e
RyanKoech <sirryankoech@gmail.com>
2022-12-17 09:48:30 +0300

    Updated package name

--- common/src/androidTest/java/com/ryankoech/krypto/common/ExampleInstrumentedTest.kt
+++ common/src/androidTest/java/com/ryankoech/krypto/common/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.ryankoech.common.krypto
+package com.ryankoech.krypto.common
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
