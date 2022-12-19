7ffb8d68aa747ec6611146633618eb9257641fb2
RyanKoech <sirryankoech@gmail.com>
2022-12-19 13:19:41 +0300

    Implemented DisplayCurrency Dto

---
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/data/dto/display_currency/DisplayCurrencyDto.kt
@@ -0,0 +1,8 @@
+package com.ryankoech.krypto.feature_home.data.dto.display_currency
+
+import com.ryankoech.krypto.common.presentation.util.DisplayCurrency
+
+data class DisplayCurrencyDto(
+    val currency : DisplayCurrency,
+    val value : Double
+)
