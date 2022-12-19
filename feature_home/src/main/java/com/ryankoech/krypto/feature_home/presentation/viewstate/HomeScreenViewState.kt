2984b0f46ffce3cef9f577f61732b9a83d0cf4af
RyanKoech <sirryankoech@gmail.com>
2022-12-19 13:35:19 +0300

    Updated viewstate to hold display currencies

--- feature_home/src/main/java/com/ryankoech/krypto/feature_home/presentation/viewstate/HomeScreenViewState.kt
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/presentation/viewstate/HomeScreenViewState.kt
@@ -1,9 +1,11 @@
 package com.ryankoech.krypto.feature_home.presentation.viewstate
 
 import com.ryankoech.krypto.common.presentation.util.ScreenState
+import com.ryankoech.krypto.feature_home.data.dto.display_currency.DisplayCurrencyDto
 import com.ryankoech.krypto.feature_home.data.dto.owned_coin.OwnedCoinDto
 
 data class HomeScreenViewState(
     val screenState: ScreenState = ScreenState.LOADING,
-    val ownedCoins : List<OwnedCoinDto> = listOf()
+    val ownedCoins : List<OwnedCoinDto> = listOf(),
+    val displayCurrencies : List<DisplayCurrencyDto> = listOf(),
 )
