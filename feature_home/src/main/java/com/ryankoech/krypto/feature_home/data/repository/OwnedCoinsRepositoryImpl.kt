8833e9011d1bcebcb731087ba329fdb12c352563
RyanKoech <sirryankoech@gmail.com>
2022-12-19 13:25:13 +0300

    Defined and implemented repository for display currency data

--- feature_home/src/main/java/com/ryankoech/krypto/feature_home/data/repository/OwnedCoinsRepositoryImpl.kt
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/data/repository/OwnedCoinsRepositoryImpl.kt
@@ -1,13 +1,17 @@
 package com.ryankoech.krypto.feature_home.data.repository
 
+import com.ryankoech.krypto.common.presentation.util.DisplayCurrency
 import com.ryankoech.krypto.feature_home.data.data_source.local.OwnedCoinsDao
+import com.ryankoech.krypto.feature_home.data.data_source.local.shared_pref.HomeLocalPref
+import com.ryankoech.krypto.feature_home.data.dto.display_currency.DisplayCurrencyDto
 import com.ryankoech.krypto.feature_home.data.dto.owned_coin.OwnedCoinDto
 import com.ryankoech.krypto.feature_home.domain.repository.OwnedCoinsRepository
 import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 class OwnedCoinsRepositoryImpl @Inject constructor(
-    private val coinsDao: OwnedCoinsDao
+    private val coinsDao: OwnedCoinsDao,
+    private val homeLocalPref: HomeLocalPref,
 ) : OwnedCoinsRepository {
 
     override suspend fun saveOwnedCoin(coin: OwnedCoinDto): Long {
@@ -19,6 +23,19 @@ class OwnedCoinsRepositoryImpl @Inject constructor(
     override suspend fun getOwnedCoins(): List<OwnedCoinDto> {
         return coroutineScope {
             coinsDao.getOwnedCoins()
+        }
+    }
+
+    override suspend fun saveDisplayCurrencyData(displayCurrencyDataMap: HashMap<DisplayCurrency, Double>) {
+        return coroutineScope {
+            homeLocalPref.saveDisplayCurrencyData(displayCurrencyDataMap)
+        }
+    }
+
+
+    override suspend fun getDisplayCurrencyData(): List<DisplayCurrencyDto>? {
+        return coroutineScope {
+            homeLocalPref.getDisplayCurrencyData()
         }
     }
 
--- feature_home/src/main/java/com/ryankoech/krypto/feature_home/domain/repository/OwnedCoinsRepository.kt
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/domain/repository/OwnedCoinsRepository.kt
@@ -1,5 +1,7 @@
 package com.ryankoech.krypto.feature_home.domain.repository
 
+import com.ryankoech.krypto.common.presentation.util.DisplayCurrency
+import com.ryankoech.krypto.feature_home.data.dto.display_currency.DisplayCurrencyDto
 import com.ryankoech.krypto.feature_home.data.dto.owned_coin.OwnedCoinDto
 
 interface OwnedCoinsRepository {
@@ -7,5 +9,9 @@ interface OwnedCoinsRepository {
     suspend fun saveOwnedCoin(coin : OwnedCoinDto) : Long
 
     suspend fun getOwnedCoins() : List<OwnedCoinDto>
+
+    suspend fun saveDisplayCurrencyData(displayCurrencyDataMap : HashMap<DisplayCurrency, Double>)
+
+    suspend fun getDisplayCurrencyData() : List<DisplayCurrencyDto>?
 
 }
