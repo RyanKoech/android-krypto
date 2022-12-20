dc774b129521c05a0bfd5a689b1908a5f9e40719
RyanKoech <sirryankoech@gmail.com>
2022-12-20 22:04:46 +0300

    Put HomeScreen together

--- feature_home/src/main/java/com/ryankoech/krypto/feature_home/presentation/HomeScreen.kt
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/presentation/HomeScreen.kt
@@ -5,20 +5,33 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.ryankoech.krypto.common.presentation.theme.KryptoTheme
+import com.ryankoech.krypto.common.presentation.util.ScreenState
+import com.ryankoech.krypto.feature_home.presentation.viewmodel.HomeScreenViewModel
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    viewModel: HomeScreenViewModel = hiltViewModel()
+) {
+
+    val viewState = viewModel.viewState.value
+
     Surface(
         modifier = Modifier
             .fillMaxSize(),
     ) {
-        Column(
-            modifier = Modifier
-                .padding(top = 12.dp, start = 12.dp, end = 12.dp)
-                .fillMaxSize()
-        ) {
+        when (viewState.screenState) {
+            ScreenState.LOADING -> {
+                HomeScreenLoading()
+            }
+            ScreenState.ERROR -> {
+                HomeScreenError()
+            }
+            ScreenState.SUCCESS -> {
+                HomeScreenSuccess(viewState.ownedCoins, viewState.displayCurrencies)
+
+            }
         }
     }
 }
