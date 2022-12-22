73c8c87733b5403e8b33c4fef849c64b6fb014a5
RyanKoech <sirryankoech@gmail.com>
2022-12-22 23:57:33 +0300

    Updated Typography

--- common/src/main/java/com/ryankoech/krypto/common/presentation/theme/Type.kt
+++ common/src/main/java/com/ryankoech/krypto/common/presentation/theme/Type.kt
@@ -8,53 +8,45 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import ke.co.sevenskies.common.R
 
-val Montserrat = FontFamily(
-    Font(R.font.montserrat_extralight, weight = FontWeight.ExtraLight),
-    Font(R.font.montserrat_light, weight = FontWeight.Light),
-    Font(R.font.montserrat_regular, weight = FontWeight.Normal),
-    Font(R.font.montserrat_medium, weight = FontWeight.Medium),
-    Font(R.font.montserrat_semibold, weight = FontWeight.SemiBold),
-    Font(R.font.montserrat_bold, weight = FontWeight.Bold),
-    Font(R.font.montserrat_extrabold, weight = FontWeight.ExtraBold),
-    Font(R.font.montserrat_black, weight = FontWeight.Black),
+val CircularStd = FontFamily(
+    Font(R.font.circular_std_book, weight = FontWeight.Medium),
+    Font(R.font.circular_std_bold, weight = FontWeight.Bold),
 )
 
-
-// Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 12.sp,
+        fontFamily = CircularStd,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
     ),
     h1 = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
-    ),
-    h2 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = CircularStd,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp,
     ),
+    h2 = TextStyle(
+        fontFamily = CircularStd,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+    ),
     h3 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = CircularStd,
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
     ),
     h4 = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = CircularStd,
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp,
     ),
     caption = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.Normal,
-        fontSize = 10.sp,
-    ),
-    button = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = CircularStd,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
+    ),
+    button = TextStyle(
+        fontFamily = CircularStd,
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp,
     ),
 )
