58ed62ff3b53196a350fe6ad515cb9c3735a9384
RyanKoech <sirryankoech@gmail.com>
2022-12-24 15:23:56 +0300

    add timber error logging in catch blocks

--- feature_home/src/main/java/com/ryankoech/krypto/feature_home/domain/usecase/WipeDatabaseUseCase.kt
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/domain/usecase/WipeDatabaseUseCase.kt
@@ -3,6 +3,7 @@ package com.ryankoech.krypto.feature_home.domain.usecase
 import com.ryankoech.krypto.common.core.util.Resource
 import com.ryankoech.krypto.feature_home.domain.repository.OwnedCoinsRepository
 import kotlinx.coroutines.flow.flow
+import timber.log.Timber
 import javax.inject.Inject
 
 class WipeDatabaseUseCase @Inject constructor(
@@ -13,6 +14,7 @@ class WipeDatabaseUseCase @Inject constructor(
             try {
                 emit(Resource.Success(repository.wipeDatabase()))
             }catch (e : Exception) {
+                Timber.e(e)
                 emit(Resource.Error(e.message?: "Unexpected Error Occurred"))
         }
     }
