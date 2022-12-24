2731562239654a6fcd6fd326d9db7d468f230c5b
RyanKoech <sirryankoech@gmail.com>
2022-12-24 15:12:43 +0300

    Implemented timber setup

--- app/src/main/java/com/ryankoech/krypto/core/KryptoApplication.kt
+++ app/src/main/java/com/ryankoech/krypto/core/KryptoApplication.kt
@@ -1,7 +1,16 @@
 package com.ryankoech.krypto.core
 
 import android.app.Application
+import com.ryankoech.krypto.core.logging.CrashAndLog
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class KryptoApplication : Application()
+class KryptoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        CrashAndLog.setupTimber()
+    }
+
+}
