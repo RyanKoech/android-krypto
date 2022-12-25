efe16fded55744b3f2eb6e46c69e0a5d6b89dd79
RyanKoech <sirryankoech@gmail.com>
2022-12-25 16:50:38 +0300

    add named annotation for repo injection

--- feature_home/src/main/java/com/ryankoech/krypto/feature_home/domain/usecase/WipeDatabaseUseCase.kt
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/domain/usecase/WipeDatabaseUseCase.kt
@@ -1,13 +1,15 @@
 package com.ryankoech.krypto.feature_home.domain.usecase
 
 import com.ryankoech.krypto.common.core.util.Resource
+import com.ryankoech.krypto.feature_home.core.di.HILT_NAME_REPO_FOR_ALL
 import com.ryankoech.krypto.feature_home.domain.repository.OwnedCoinsRepository
 import kotlinx.coroutines.flow.flow
 import timber.log.Timber
 import javax.inject.Inject
+import javax.inject.Named
 
 class WipeDatabaseUseCase @Inject constructor(
-    private val repository: OwnedCoinsRepository
+    @Named(HILT_NAME_REPO_FOR_ALL) private val repository: OwnedCoinsRepository
 ) {
 
     operator fun invoke() = flow {
