12546e49d08dacdc2ce7e1e3975aa5131ede7821
RyanKoech <sirryankoech@gmail.com>
2023-01-04 07:40:33 +0300

    use flow catch block

--- feature_home/src/main/java/com/ryankoech/krypto/feature_home/domain/usecase/WipeDatabaseUseCase.kt
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/domain/usecase/WipeDatabaseUseCase.kt
@@ -3,6 +3,7 @@ package com.ryankoech.krypto.feature_home.domain.usecase
 import com.ryankoech.krypto.common.core.util.Resource
 import com.ryankoech.krypto.feature_home.core.di.HILT_NAME_REPO_FOR_ALL
 import com.ryankoech.krypto.feature_home.domain.repository.OwnedCoinsRepository
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import timber.log.Timber
 import javax.inject.Inject
@@ -12,12 +13,10 @@ class WipeDatabaseUseCase @Inject constructor(
     @Named(HILT_NAME_REPO_FOR_ALL) private val repository: OwnedCoinsRepository
 ) {
 
-    operator fun invoke() = flow {
-            try {
-                emit(Resource.Success(repository.wipeDatabase()))
-            }catch (e : Exception) {
-                Timber.e(e)
-                emit(Resource.Error(e.message?: "Unexpected Error Occurred"))
-        }
+    operator fun invoke() = flow<Resource<Unit>> {
+        emit(Resource.Success(repository.wipeDatabase()))
+    }.catch { e ->
+        Timber.e(e)
+        emit(Resource.Error(e.message?: "Unexpected Error Occurred"))
     }
 }
