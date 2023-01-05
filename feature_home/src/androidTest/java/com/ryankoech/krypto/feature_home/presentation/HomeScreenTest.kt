71c3e12b04233618fe38e3ff254c0873370b6c21
RyanKoech <sirryankoech@gmail.com>
2023-01-05 08:58:23 +0300

    add wipe wallet functionality assertion

--- feature_home/src/androidTest/java/com/ryankoech/krypto/feature_home/presentation/HomeScreenTest.kt
+++ feature_home/src/androidTest/java/com/ryankoech/krypto/feature_home/presentation/HomeScreenTest.kt
@@ -1,24 +1,24 @@
 package com.ryankoech.krypto.feature_home.presentation
 
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.ryankoech.krypto.common.core.util.Resource
 import com.ryankoech.krypto.common.presentation.theme.KryptoTheme
+import com.ryankoech.krypto.feature_home.R
 import com.ryankoech.krypto.feature_home.core.util.EXCEPTION_MESSAGE
 import com.ryankoech.krypto.feature_home.data.dto.owned_coin.OwnedCoinDto
-import com.ryankoech.krypto.feature_home.data.repository.FakeOwnedCoins
 import com.ryankoech.krypto.feature_home.data.repository.FakeOwnedCoinsRepositoryImpl
 import com.ryankoech.krypto.feature_home.domain.usecase.GetDisplayCurrencyDataUseCase
 import com.ryankoech.krypto.feature_home.domain.usecase.GetOwnedCoinsUseCase
 import com.ryankoech.krypto.feature_home.domain.usecase.WipeDatabaseUseCase
+import com.ryankoech.krypto.feature_home.presentation.components.success.HOME_SCREEN_ACTION_ITEM_WIPE_WALLET
 import com.ryankoech.krypto.feature_home.presentation.viewmodel.HomeScreenViewModel
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit4.MockKRule
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.test.runTest
 
 import org.junit.Before
 import org.junit.Rule
@@ -27,7 +27,7 @@ import org.junit.Test
 class HomeScreenTest {
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @get:Rule
     val mockkRule = MockKRule(this)
@@ -116,6 +116,29 @@ class HomeScreenTest {
         composeTestRule.onNodeWithTag(TEST_TAG_HOME_SCREEN_SUCCESS).assertIsDisplayed()
     }
 
+    @Test
+    fun clickConfirmWipeWallet_closeConfirmDialog() {
+        getOwnedCoinsUseCase = GetOwnedCoinsUseCase(FakeOwnedCoinsRepositoryImpl())
+        wipeDatabaseUseCase = WipeDatabaseUseCase(FakeOwnedCoinsRepositoryImpl())
 
+        viewModel = HomeScreenViewModel(
+            getOwnedCoinsUseCase,
+            getDisplayCurrencyDataUseCase,
+            wipeDatabaseUseCase
+        )
+
+        composeTestRule.setContent {
+            KryptoTheme {
+                HomeScreen(onTransferInClick = {}, onTransferOutClick = {}, viewModel)
+            }
+        }
+
+        composeTestRule.onNodeWithTag(HOME_SCREEN_ACTION_ITEM_WIPE_WALLET).onChildAt(0).performClick()
+
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.button_text_wipe_yes)).performClick()
+
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.home_screen_success_not_data_message)).assertIsDisplayed()
+
+    }
 
 }
