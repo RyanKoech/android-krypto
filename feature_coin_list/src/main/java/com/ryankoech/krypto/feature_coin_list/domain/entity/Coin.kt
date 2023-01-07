9f43c421d56db3ebfa22ba1ce7f55f644b0f1847
RyanKoech <sirryankoech@gmail.com>
2023-01-07 13:49:26 +0300

    defined entity

---
+++ feature_coin_list/src/main/java/com/ryankoech/krypto/feature_coin_list/domain/entity/Coin.kt
@@ -0,0 +1,14 @@
+package com.ryankoech.krypto.feature_coin_list.domain.entity
+
+data class Coin(
+    val id : String,
+    val symbol : String,
+    val marketCapRank : Int,
+    val image : String,
+    val change : Float,
+    val price : Double,
+    val marketCap : Long,
+    val allTimeHigh : Double,
+    val high24Hr : Double,
+    val totalVolume : Long,
+)
