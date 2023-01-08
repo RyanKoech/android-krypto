b304bd3945154482dcb9144c63bdbfdb18eb7b37
RyanKoech <sirryankoech@gmail.com>
2023-01-08 14:23:51 +0300

    add screen state logic

--- feature_coin_list/src/main/java/com/ryankoech/krypto/feature_coin_list/presentation/CoinListScreen.kt
+++ feature_coin_list/src/main/java/com/ryankoech/krypto/feature_coin_list/presentation/CoinListScreen.kt
@@ -5,15 +5,34 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
 import com.ryankoech.krypto.common.presentation.theme.KryptoTheme
+import com.ryankoech.krypto.common.presentation.util.ScreenState
+import com.ryankoech.krypto.feature_coin_list.presentation.viewmodel.CoinListScreenViewModel
 
 @Composable
-fun CoinListScreen() {
+fun CoinListScreen(
+    viewModel: CoinListScreenViewModel = hiltViewModel(),
+    modifier: Modifier = Modifier
+) {
+
+    val viewState = viewModel.viewState.value
 
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-        CoinListScreenSuccess()
+        when(viewState.screenState){
+            ScreenState.LOADING -> {
+                //TODO : Show Loading Screen
+            }
+            ScreenState.ERROR -> {
+                //TODO : Show Error Screen
+            }
+            ScreenState.SUCCESS -> {
+                CoinListScreenSuccess()
+            }
+        }
     }
 }
 
