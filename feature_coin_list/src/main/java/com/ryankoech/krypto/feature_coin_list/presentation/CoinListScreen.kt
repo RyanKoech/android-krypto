c652736509c2bfadcf8a85122c8c9698a3af96a5
RyanKoech <sirryankoech@gmail.com>
2023-01-12 12:12:30 +0300

    implement pull to refresh

--- feature_coin_list/src/main/java/com/ryankoech/krypto/feature_coin_list/presentation/CoinListScreen.kt
+++ feature_coin_list/src/main/java/com/ryankoech/krypto/feature_coin_list/presentation/CoinListScreen.kt
@@ -1,8 +1,14 @@
 package com.ryankoech.krypto.feature_coin_list.presentation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -12,7 +18,11 @@ import com.ryankoech.krypto.feature_coin_list.domain.entity.Order
 import com.ryankoech.krypto.feature_coin_list.domain.entity.SortCoinBy
 import com.ryankoech.krypto.feature_coin_list.domain.entity.SortInfo
 import com.ryankoech.krypto.feature_coin_list.presentation.viewmodel.CoinListScreenViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CoinListScreen(
     viewModel: CoinListScreenViewModel = hiltViewModel(),
@@ -20,6 +30,8 @@ fun CoinListScreen(
 ) {
 
     val viewState = viewModel.viewState.value
+    val refreshScope = rememberCoroutineScope()
+
     var sortInfoState by remember {
         mutableStateOf(
             SortInfo(
@@ -37,6 +49,22 @@ fun CoinListScreen(
         viewModel.getCoins(sortInfoState, subString)
         filterStringState = subString
     }
+
+    var isRefreshingState by remember {
+        mutableStateOf(false)
+    }
+
+    fun refresh() = refreshScope.launch {
+        isRefreshingState = true
+        viewModel.getCoins(sortInfoState, filterStringState)
+        delay(500)
+        isRefreshingState = false
+    }
+
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = isRefreshingState,
+        onRefresh = ::refresh
+    )
 
     fun editSortingInfo(sortCoinBy: SortCoinBy) {
 
@@ -68,14 +96,29 @@ fun CoinListScreen(
         if(viewState.screenState == ScreenState.ERROR) {
             CoinListScreenError(onButtonClick = { viewModel.getCoins(sortInfoState, filterStringState) })
         }else {
-            CoinListScreenSuccess(
-                screenState = viewState.screenState,
-                editSortInfo = ::editSortingInfo,
-                sortInfoState = sortInfoState,
-                coins = viewState.coins,
-                filterStringState = filterStringState,
-                editFilterStringState = ::editFilterStringState
-            )
+
+            Box(
+                modifier = Modifier
+                    .pullRefresh(pullRefreshState)
+            ){
+
+                CoinListScreenSuccess(
+                    screenState = viewState.screenState,
+                    editSortInfo = ::editSortingInfo,
+                    sortInfoState = sortInfoState,
+                    coins = viewState.coins,
+                    filterStringState = filterStringState,
+                    editFilterStringState = ::editFilterStringState
+                )
+
+                PullRefreshIndicator(
+                    modifier = Modifier
+                        .align(Alignment.TopCenter),
+                    refreshing = isRefreshingState,
+                    state = pullRefreshState,
+                )
+
+            }
         }
     }
 }
