5631043492a198c7da552f01d0efb5bb360b9f8e
RyanKoech <sirryankoech@gmail.com>
2023-01-12 09:14:28 +0300

    implement database and dao

---
+++ feature_coin_list/src/main/java/com/ryankoech/krypto/feature_coin_list/data/data_source/local/db/CoinDao.kt
@@ -0,0 +1,18 @@
+package com.ryankoech.krypto.feature_coin_list.data.data_source.local.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.ryankoech.krypto.feature_coin_list.data.dto.CoinLocalDto
+import com.ryankoech.krypto.feature_coin_list.data.dto.TANLENAME_COIN_DTO
+
+@Dao
+interface CoinDao {
+
+    @Insert
+    suspend fun insertCoins(coins : List<CoinLocalDto>)
+
+    @Query("SELECT * FROM $TANLENAME_COIN_DTO")
+    suspend fun getCoins() : List<CoinLocalDto>
+
+}
