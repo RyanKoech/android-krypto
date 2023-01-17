d746fd90a7af258237f4a4ca18d9abdcff5e9db3
RyanKoech <sirryankoech@gmail.com>
2023-01-17 14:45:31 +0300

    implement component

---
+++ feature_transaction/src/main/java/com/ryankoech/krypto/feature_transaction/presentation/choose_asset/components/loading/ChooseAssetItemLoading.kt
@@ -0,0 +1,66 @@
+package com.ryankoech.krypto.feature_transaction.presentation.choose_asset.components.loading
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ryankoech.krypto.common.presentation.components.loading.CoinCardLoading
+import com.ryankoech.krypto.common.presentation.components.loading.LoadingText
+import com.ryankoech.krypto.common.presentation.components.loading.loadingEffect
+import com.ryankoech.krypto.common.presentation.theme.KryptoTheme
+
+@Composable
+fun ChooseAssetItemLoading(
+    brush : Brush,
+    modifier: Modifier = Modifier,
+) {
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Spacer(
+            modifier = Modifier
+                .size(56.dp)
+                .background(
+                    brush = brush,
+                    shape = MaterialTheme.shapes.small
+                )
+        )
+
+        Spacer(
+            modifier = Modifier
+            .width(16.dp)
+        )
+
+        LoadingText(130.dp, brush)
+
+    }
+
+}
+
+@Preview
+@Composable
+fun ChooseAssetItemLoadingPreview(){
+    KryptoTheme {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            val brush = loadingEffect()
+
+            Column{
+                ChooseAssetItemLoading(brush = brush)
+            }
+
+        }
+    }
+}
