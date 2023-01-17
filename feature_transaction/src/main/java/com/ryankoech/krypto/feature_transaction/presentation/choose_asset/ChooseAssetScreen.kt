63bd1028699db99ce57eac062efa8cbc005e9c76
RyanKoech <sirryankoech@gmail.com>
2023-01-17 14:04:27 +0300

    refactored file location

--- feature_transaction/src/main/java/com/ryankoech/krypto/feature_transaction/presentation/choose_asset/ChooseAssetScreen.kt
+++ feature_transaction/src/main/java/com/ryankoech/krypto/feature_transaction/presentation/choose_asset/ChooseAssetScreen.kt
@@ -1,9 +1,5 @@
-package com.ryankoech.krypto.feature_transaction.presentation
+package com.ryankoech.krypto.feature_transaction.presentation.choose_asset
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -11,7 +7,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ryankoech.krypto.common.presentation.theme.KryptoTheme
 import com.ryankoech.krypto.common.presentation.util.ScreenState
-import com.ryankoech.krypto.feature_transaction.presentation.choose_asset.ChooseAssetScreenSuccess
 import com.ryankoech.krypto.feature_transaction.presentation.choose_asset.viewmodel.ChooseAssetScreenViewModel
 
 @Composable
