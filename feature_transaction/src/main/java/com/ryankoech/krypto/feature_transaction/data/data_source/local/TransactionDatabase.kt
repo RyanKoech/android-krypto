4128c869bd82b99d4a25c4532023ba4c71a686d2
RyanKoech <sirryankoech@gmail.com>
2023-02-12 11:50:04 +0300

    fix wrong entity class

--- feature_transaction/src/main/java/com/ryankoech/krypto/feature_transaction/data/data_source/local/TransactionDatabase.kt
+++ feature_transaction/src/main/java/com/ryankoech/krypto/feature_transaction/data/data_source/local/TransactionDatabase.kt
@@ -2,11 +2,12 @@ package com.ryankoech.krypto.feature_transaction.data.data_source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.ryankoech.krypto.feature_transaction.data.dto.transaction_dto.TransactionDto
 
 const val NAME_DATABASE_TRANSACTION = "transaction db"
 
 @Database(
-    entities = [TransactionDao::class],
+    entities = [TransactionDto::class],
     version = 1,
     exportSchema = false,
 )
