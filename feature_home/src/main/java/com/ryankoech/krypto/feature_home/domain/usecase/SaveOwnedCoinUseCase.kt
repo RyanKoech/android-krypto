f6e61bbccf58679cade74083da51694266af5a15
RyanKoech <sirryankoech@gmail.com>
2023-02-13 12:13:20 +0300

    fix usecase to return flow

--- feature_home/src/main/java/com/ryankoech/krypto/feature_home/domain/usecase/SaveOwnedCoinUseCase.kt
+++ feature_home/src/main/java/com/ryankoech/krypto/feature_home/domain/usecase/SaveOwnedCoinUseCase.kt
@@ -5,6 +5,8 @@ import com.ryankoech.krypto.feature_home.core.di.HILT_NAME_REPO_FOR_ALL
 import com.ryankoech.krypto.feature_home.data.dto.owned_coin.OwnedCoinDto
 import com.ryankoech.krypto.feature_home.domain.repository.OwnedCoinsRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
@@ -14,14 +16,11 @@ class SaveOwnedCoinUseCase @Inject constructor(
     @Named(HILT_NAME_REPO_FOR_ALL) private val repository : OwnedCoinsRepository
 ) {
 
-    suspend operator fun invoke(coin : OwnedCoinDto) : Resource<String> =
-        try {
-            val id = repository.saveOwnedCoin(coin)
-            Resource.Success(
-                data = id
-            )
-        }catch (e : Exception) {
-            Timber.e(e)
-            Resource.Error(e.message?: "Unexpected Error Occurred" )
-        }
+    operator fun invoke(coin : OwnedCoinDto) = flow<Resource<String>> {
+        val id = repository.saveOwnedCoin(coin)
+        emit(Resource.Success(id))
+    }.catch { e ->
+        Timber.e(e)
+        emit(Resource.Error(e.message?: "Unexpected Error Occurred" ))
+    }
 }
