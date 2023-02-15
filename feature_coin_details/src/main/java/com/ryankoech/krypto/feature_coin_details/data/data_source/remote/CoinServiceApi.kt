3b67010d655190ece9f807161fa22229e2087e7b
RyanKoech <sirryankoech@gmail.com>
2023-02-15 12:48:30 +0300

    implement api serivce with dto

---
+++ feature_coin_details/src/main/java/com/ryankoech/krypto/feature_coin_details/data/data_source/remote/CoinServiceApi.kt
@@ -0,0 +1,18 @@
+package com.ryankoech.krypto.feature_coin_details.data.data_source.remote
+
+import com.ryankoech.krypto.feature_coin_details.data.dto.market_chart_dto.MarketChartDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface CoinServiceApi {
+
+    @GET("/coins/{id}/market_chart")
+    suspend fun getCoinMarketChart(
+        @Path(value = "id", encoded = true) coinId : String,
+        @Query("days") days : Int,
+        @Query("vs_currency") currency : String = "usd",
+    ) : Response<MarketChartDto>
+
+}
