8d633e048547af5f7926d8fd38a6bf6df8466f01
RyanKoech <sirryankoech@gmail.com>
2023-02-20 11:03:18 +0300

    refactored api service class name

--- feature_coin_details/src/main/java/com/ryankoech/krypto/feature_coin_details/data/data_source/remote/CoinDetailsServiceApi.kt
+++ feature_coin_details/src/main/java/com/ryankoech/krypto/feature_coin_details/data/data_source/remote/CoinDetailsServiceApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface CoinServiceApi {
+interface CoinDetailsServiceApi {
 
     @GET("/coins/{id}/market_chart")
     suspend fun getCoinMarketChart(
