2fdd16f425507c04da126ec6ab8a7b0e0664b03f
RyanKoech <sirryankoech@gmail.com>
2023-02-28 12:47:06 +0300

    implement logic to get transactions

--- feature_coin_details/src/main/java/com/ryankoech/krypto/feature_coin_details/presentation/viewmodel/CoinDetailsScreenViewModel.kt
+++ feature_coin_details/src/main/java/com/ryankoech/krypto/feature_coin_details/presentation/viewmodel/CoinDetailsScreenViewModel.kt
@@ -8,18 +8,24 @@ import com.ryankoech.krypto.common.core.util.Resource
 import com.ryankoech.krypto.common.presentation.util.ScreenState
 import com.ryankoech.krypto.feature_coin_details.domain.usecase.GetCoinMarketChartUseCase
 import com.ryankoech.krypto.feature_coin_details.presentation.viewstate.CoinDetailsScreenViewState
+import com.ryankoech.krypto.feature_transaction.domain.usecase.GetCoinTransactionsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
 class CoinDetailsScreenViewModel @Inject constructor(
-    private val getCoinMarketChartUseCase: GetCoinMarketChartUseCase
+    private val getCoinMarketChartUseCase: GetCoinMarketChartUseCase,
+    private val getCoinTransactionsUseCase: GetCoinTransactionsUseCase,
 ) : ViewModel(){
 
     private val _viewState = mutableStateOf(CoinDetailsScreenViewState())
-    private val viewState : State<CoinDetailsScreenViewState> = _viewState
+    val viewState : State<CoinDetailsScreenViewState> = _viewState
+    private val _message = MutableSharedFlow<String>()
+    val message : SharedFlow<String> get() = _message
 
     fun getCoinDetails(coinId :String) {
 
@@ -45,6 +51,30 @@ class CoinDetailsScreenViewModel @Inject constructor(
                         )
                     }
                 }
+
+            }.launchIn(viewModelScope)
+
+    }
+
+    fun getTransactions(coinId: String) {
+
+        getCoinTransactionsUseCase(coinId)
+            .onEach { res ->
+                   
+                when(res){
+                    is Resource.Error -> {
+                        _message.emit("An error occurred while fetching transactions.")
+                    }
+                    is Resource.Loading -> {
+                        // Do nothing
+                    }
+                    is Resource.Success -> {
+                        _viewState.value = _viewState.value.copy(
+                            transactions = res.data!!
+                        )
+                    }
+                }
+
 
             }.launchIn(viewModelScope)
 
