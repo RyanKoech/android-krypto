3b9eba4ced209279c1c6f971d0d7cea704f22a5f
RyanKoech <sirryankoech@gmail.com>
2023-02-28 12:47:06 +0300

    implement logic to get transactions

--- feature_coin_details/src/main/java/com/ryankoech/krypto/feature_coin_details/presentation/viewstate/CoinDetailsScreenViewState.kt
+++ feature_coin_details/src/main/java/com/ryankoech/krypto/feature_coin_details/presentation/viewstate/CoinDetailsScreenViewState.kt
@@ -2,10 +2,12 @@ package com.ryankoech.krypto.feature_coin_details.presentation.viewstate
 
 import com.ryankoech.krypto.common.presentation.util.ScreenState
 import com.ryankoech.krypto.feature_coin_details.domain.entity.MarketChartEntity
+import com.ryankoech.krypto.feature_transaction.data.dto.transaction_dto.TransactionDto
 
 data class CoinDetailsScreenViewState(
     private val screenState: ScreenState = ScreenState.LOADING,
     private val dayMarketChart : List<MarketChartEntity> = listOf(),
     private val threeMonthMarketChart : List<MarketChartEntity> = listOf(),
     private val yearMarketChart : List<MarketChartEntity> = listOf(),
+    private val transactions : List<TransactionDto> = listOf(),
 )
