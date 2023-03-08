f8d7e92311d6aed969086fc33182d03ed13af51f
RyanKoech <sirryankoech@gmail.com>
2023-03-08 12:48:14 +0300

    fix navigation bug

--- app/src/main/java/com/ryankoech/krypto/presentation/MainActivity.kt
+++ app/src/main/java/com/ryankoech/krypto/presentation/MainActivity.kt
@@ -70,17 +70,10 @@ class MainActivity : ComponentActivity() {
 
             fun navigateToHomeScreen() {
                 navController.navigate(Screens.Home.route) {
-                    // Pop up to the start destination of the graph to
-                    // avoid building up a large stack of destinations
-                    // on the back stack as users select items
                     popUpTo(navController.graph.findStartDestination().id) {
-                        saveState = true
+                        inclusive = true
                     }
-                    // Avoid multiple copies of the same destination when
-                    // reselecting the same item
                     launchSingleTop = true
-                    // Restore state when reselecting a previously selected item
-                    restoreState = true
                 }
             }
 
