f22f5fbd4b53f16ff24f8261f7aec4eb1eab0a84
RyanKoech <sirryankoech@gmail.com>
2023-03-16 13:44:19 +0300

    implement setting items functionalities

--- feature_settings/src/main/java/com/ryankoech/krypto/feature_settings/core/ktx/Context.kt
+++ feature_settings/src/main/java/com/ryankoech/krypto/feature_settings/core/ktx/Context.kt
@@ -8,12 +8,38 @@ import android.widget.Toast
 
 fun Context.sendMail(to: String, subject: String, body : String) {
     try {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("mailto:$to"))
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:") // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(to))
         intent.putExtra(Intent.EXTRA_SUBJECT, subject)
         intent.putExtra(Intent.EXTRA_TEXT, body)
         startActivity(intent)
     } catch (e: ActivityNotFoundException) {
         Toast.makeText(this, "No Mail App Found", Toast.LENGTH_SHORT).show()
+    } catch (t: Throwable) {
+        Toast.makeText(this, "Something Went Wrong", Toast.LENGTH_SHORT).show()
+    }
+}
+
+fun Context.sendMessage(message : String) {
+    try {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, message)
+        startActivity(intent)
+    } catch (e: ActivityNotFoundException) {
+        Toast.makeText(this, "Messaging App Found", Toast.LENGTH_SHORT).show()
+    } catch (t: Throwable) {
+        Toast.makeText(this, "Something Went Wrong", Toast.LENGTH_SHORT).show()
+    }
+}
+
+fun Context.rateInPlaystore() {
+    try {
+        val intent = Intent(Intent.ACTION_VIEW,  Uri.parse("market://details?id=${this.packageName}"))
+        startActivity(intent)
+    } catch (e: ActivityNotFoundException) {
+        Toast.makeText(this, "PlayStore App Found", Toast.LENGTH_SHORT).show()
     } catch (t: Throwable) {
         Toast.makeText(this, "Something Went Wrong", Toast.LENGTH_SHORT).show()
     }
--- feature_settings/src/main/java/com/ryankoech/krypto/feature_settings/presentation/SettingsScreen.kt
+++ feature_settings/src/main/java/com/ryankoech/krypto/feature_settings/presentation/SettingsScreen.kt
@@ -1,54 +1,79 @@
 package com.ryankoech.krypto.feature_settings.presentation
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ryankoech.krypto.common.presentation.theme.KryptoTheme
 import com.ryankoech.krypto.feature_settings.R
+import com.ryankoech.krypto.feature_settings.core.ktx.rateInPlaystore
+import com.ryankoech.krypto.feature_settings.core.ktx.sendMail
+import com.ryankoech.krypto.feature_settings.core.ktx.sendMessage
 import com.ryankoech.krypto.feature_settings.presentation.components.SettingsItem
 import com.ryankoech.krypto.feature_settings.presentation.util.SettingsEntity
 
 @Composable
 fun SettingsScreen(
     modifier : Modifier = Modifier,
+    navigateToAboutScreen : () -> Unit,
 ) {
+
+    val context = LocalContext.current
+    val uriHandler = LocalUriHandler.current
 
     val settingsList = listOf(
         SettingsEntity(
             title = "App Info",
             details = "Learn more about Krypto",
             iconRes = R.drawable.icon_info,
-            action = {}
+            action = navigateToAboutScreen
         ),
         SettingsEntity(
             title = "Report An Issue",
             details = "Tell us what wrong with the app",
             iconRes = R.drawable.icon_bug,
-            action = {}
+            action = {
+                context.sendMail(
+                    to = "sirryankoech@gmail.com",
+                    subject = "Krypto - Report a Problem",
+                    body = "Device Information:\n\nYour Issue:"
+                )
+            }
         ),
         SettingsEntity(
             title = "Share Krypto",
             details = "Help other people discover the app",
             iconRes = R.drawable.icon_share,
-            action = {}
+            action = {
+                context.sendMessage(
+                    message = "A simple way to manager crypto assets and practice crypto trading. By Ryan and the android community.\n\nRelease coming soon.",
+                )
+            }
         ),
         SettingsEntity(
             title = "Rate Our App",
             details = "Leave a review if you loved Krypto",
             iconRes = R.drawable.icon_star,
-            action = {}
+            action = {
+                context.rateInPlaystore()
+            }
         ),
         SettingsEntity(
             title = "Buy Me Coffee",
             details = "Help support further development",
             iconRes = R.drawable.icon_gift,
-            action = {}
+            action = {
+                uriHandler.openUri("https://www.buymeacoffee.com/RyanKoech")
+            }
         ),
     )
 
@@ -63,6 +88,7 @@ fun SettingsScreen(
             SettingsItem(
                 onClick = settingListItem.action, iconRes = settingListItem.iconRes, title = settingListItem.title, details = settingListItem.details
             )
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 
@@ -73,7 +99,7 @@ fun SettingsScreen(
 fun SettingsScreenPreview() {
     KryptoTheme{
         Surface {
-            SettingsScreen()
+            SettingsScreen(Modifier, {})
         }
     }
 }
